9e43876301e90b6471d87643439f9b1522b14661
Benbriel <benjamin.briceno.bb@gmail.com>
2022-11-14 04:01:11 -0300

    el mejor push de la historia

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 from utils import *
+from tqdm import tqdm
 
-U_guess = 0
+U_guess = 0 * eV
+E_ = np.linspace(E_C + U_guess, 50*eV, 50000)
 T = 300
-V_DS = 0
-V_G = 0
-# Emin = E_C + U_guess
-Emax = 100 * np.abs(E_C)
-args = (Emax, T, V_DS, V_G)
+V_DS = .5
+V_G = .5
+args = (T, V_DS, V_G)
 
-U_array = []
-for i in range(20):
-    U_new = iter_alg(U_guess, E_C + U_guess, *args)
-    print('U_new =', U_new)
-    U_guess = U_new
-    U_array.append(U_new)
-
-plt.plot(U_array)
-plt.yscale('symlog')
+U_array = np.linspace(-100, -1, 100) * eV
+for VG in V_G_array:
+    plt.plot(V_DS_array, [calculate_I(E_, -VG, T, -V_DS) for V_DS in V_DS_array])
 plt.show()
+#print(calculate_I(E_, U_guess, T, V_DS))
+#breakpoint()
+N_array = []
+N0_array = []
+U_array = []
+for i in tqdm(range(1000)):
+    N, N0, U_new = iter_alg(U_guess, *args)
+    U_guess = U_new
+    N_array.append(N)
+    N0_array.append(N0)
+    U_array.append(U_guess / eV)
 
-breakpoint()
+print(f'N0 = {N0:.3e}, N = {N:.3e}')
+print(f"U = {U_new / eV:.6f} eV")
 
-calculate_N(E_C+U_guess, Emax, 300, U_guess, V_DS_array[-1])
-
-breakpoint()
+fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+ax[0].plot(N_array[1:], '.')
+ax[0].set_title("N")    
+ax[1].plot(N0_array[1:], '.')
+ax[1].set_title("N0")
+ax[2].plot(U_array[1:], '.')
+ax[2].set_title("U")
+plt.show()
--- utils.py
+++ utils.py
@@ -4,13 +4,12 @@ from scipy.constants import k, e, hbar, eV
 import matplotlib.pyplot as plt
 
 # Define Constants
-# hbar /= eV                                  # eV*s
 nm = 1e-9                                   # m
 Length = 40 * nm                            # m
 Width = 3 * Length                          # m
 A = Length * Width                          # m^2
-E_F = -5. * eV                                   # eV
-E_C = -4.7 * eV                                  # eV
+E_F = -5. * eV                              # J
+E_C = -4.7 * eV                             # J
 # tau_d = tau_s -> 0. tal que tau_d / tau_s -> 1
 C_es = C_G = 0.1 * 1e-15                    # F
 T_array = np.array([1., 298])               # K
@@ -19,12 +18,10 @@ V_G_array = np.array([.3, .35, .4, .45, .5])# V
 m_eff = 9.1 / 2 * 1e-31                     # kg
 
 # Algunas variables
-beta = lambda T: 1/(k*T)               # eV^-1
+beta = lambda T: 1/(k*T)               # J^-1
 mu_s = E_F
-mu_d = lambda _V_DS: E_F * eV + e*_V_DS     # eV
+mu_d = lambda _V_DS: E_F + e*_V_DS     # J
 
-# Para testear
-E_plot = np.linspace(-6, -2, 100000, dtype=np.longdouble)
 
 def g(E):
     """
@@ -32,7 +29,7 @@ def g(E):
     :param E: Es la energía para la cual se quiere calcular la función de densidad de estados
     :return: devuelve un np.array con los resultados de la entrada que se le da
     """
-    return A * m_eff / (np.pi * hbar**2) * (E > 0)
+    return A * m_eff / (np.pi * hbar**2) * (E >= 0)
 
 
 def f(E, T):
@@ -42,23 +39,26 @@ def f(E, T):
     :param mu: Potencial químico en eV
     :return: valor de la función FD np.array
     """
-    neg_exp = np.exp(-E * beta(T))
-    return neg_exp / (1 + neg_exp)
+    return np.piecewise(E, [E < 0, E >= 0],
+    [
+        lambda E: 1 / (np.exp(beta(T) * E) + 1),
+        lambda E: np.exp(-beta(T) * E) / (np.exp(-beta(T) * E) + 1)
+        ]
+    )
 
 
-def integrate(integrand, min_value, max_value):
+def integrate(integrand, x):
     """
     Esta función integra cualquier función capaz de recibir un escalar o np.array y la integra en los
-    límites especificados
+    límites especificados usando el método de trapecios
     :param integrand: es el objeto de python que representa la función a integrar
     :param min_value: es el límite inf de la integral
     :param max_value: es el lim sup de la integral
     :return: devuelve un escalar que es el valor de la integral
     """
-    res, err = quad(integrand, min_value, max_value)
-    return res
+    return np.trapz(integrand, x=x)
 
-def calculate_N(E_min, E_max, T, U, V_DS):
+def calculate_N(E: np.ndarray|float, T, U, V_DS):
     """
     Esta función calcula el valor de N según la ecuación (5.13), (5.15) y (5.16) del libro del curso
     :param E_min: la energía mínima por donde partir la integral, en rigor es menos infinito pero
@@ -71,15 +71,18 @@ def calculate_N(E_min, E_max, T, U, V_DS):
     :param mu_d: potencial químido drain en eV
     :return: devuelve el valor de N calculado a partir de estadisticas Fermi-Dirac
     """
-    integrand1 = lambda E: g(E-E_C-U) * f(E - mu_s, T)
-    integrand2 = lambda E: g(E-E_C-U) * f(E - mu_d(V_DS), T)
-    integral1 = integrate(integrand1, E_min, E_max)
-    integral2 = integrate(integrand2, E_min, E_max)
-    return (integral1 + integral2) / 2
+    integrand1 = g(E-E_C-U) * f(E - mu_s, T)
+    integrand2 = g(E-E_C-U) * f(E - mu_d(V_DS), T)
+    integrand = (integrand1 + integrand2) / 2
+    #plt.plot(E, integrand)
+    #plt.plot(E, g(E-E_C-U))
+    #plt.plot(E, f(E - mu_s, T) + f(E - mu_d(V_DS), T))
+    #breakpoint()
+    return integrate(integrand, x=E)
 
 
-def calculate_N0(T):
-    return integrate(lambda E: g(E-E_C) * f(E - E_F, T), E_C, np.inf)
+def calculate_N0(E: np.ndarray|float, T):
+    return integrate(g(E-E_C) * f(E - E_F, T), x=E)
 
 
 def calculate_U(N, N0, V_G):
@@ -89,7 +92,7 @@ def calculate_U(N, N0, V_G):
     :param N: Número de electrones
     :return: Un potencial U actualizado para ser más consistente con N
     """
-    return -V_G * e + e ** 2 * (N - N0) / (C_es)
+    return -V_G * e + e ** 2 * (N - N0) / C_es
 
 
 def convergence_criteria(value_before, value_now, th=1e-6):
@@ -100,20 +103,25 @@ def convergence_criteria(value_before, value_now, th=1e-6):
     :param th: es el umbral de la diferencia para evaluar convergencia
     :return: devuelve True si hay convergencia y False en caso contrario
     """
-    if np.abs(value_before - value_now) < th:
-        return True
-    else:
-        return False
+    return np.abs(value_before - value_now) < th
 
-def iter_alg(U, Emin, Emax, T, V_DS, V_G):
+def iter_alg(U, T, V_DS, V_G):
     """
     Esta es una iteración del algoritmo, recibe una guess y devuelve un U nuevo
     :param U: U en la iteración anterior
     :return: U nuevo
     """
-    N = calculate_N(Emin, Emax, T, U, V_DS)
-    print('N: ', N)
-    N0 = calculate_N0(T)
-    print('N0: ', N0)
+    Emin = (E_C + U)
+    Emax = 10*eV
+    E_N = np.linspace(Emin, Emax, 5000)
+    E_N0 = np.linspace(E_C, Emax, 5000)
+    N = calculate_N(E_N, T, U, V_DS)
+    N0 = calculate_N0(E_N0, T)
     U_new = calculate_U(N, N0, V_G)
-    return U_new
+    return N, N0, U_new
+
+def calculate_I(E: np.ndarray|float, U, T, V_DS):
+    integrand1 = np.sqrt(2*m_eff*(E-E_C-U), where=E-E_C-U>=0) * (E-E_C-U >= 0)
+    integrand2 =  f(E - mu_s, T) - f(E - mu_d(V_DS), T)
+    integrand = integrand1 * integrand2
+    return integrate(integrand, x=E) * e * Width / (np.pi * hbar)**2
