50772188a0458afcd1f50577ca218372ba7ead9a
Benbriel <benjamin.briceno.bb@gmail.com>
2022-11-21 08:09:51 -0300

    holiis

--- main.py
+++ main.py
@@ -16,7 +16,17 @@ U_guess = -0.3*eV
 U_iter, U = load_U_iter('U_iter_a', T_arr, V_DS_arr, V_G_arr, U_guess=U_guess, n_iter=n_iter)
 I = get_I(U, T_arr, V_DS_arr)
 
-# plot_U(U, U_iter)
+plt.figure(figsize=(5, 5))
+plt.title('$V_G$ = $0.5$ V, $V_{DS}$ = $0.5$ V')
+plt.plot(U_iter[:, 0, -1, -1]/eV, label='T = 1 K')
+plt.plot(U_iter[:, 1, -1, -1]/eV, label='T = 298 K')
+plt.legend()
+plt.grid()
+plt.xlabel('Iteración')
+plt.ylabel('U [eV]')
+plt.tight_layout()
+plt.show()
+plt.savefig('fig_a_iter.png', dpi=300)
 
 fig, ax = plt.subplots(1, 2, figsize=(10, 5), tight_layout=True, sharey=True)
 for j, T in enumerate(T_arr):
@@ -29,7 +39,7 @@ for j, T in enumerate(T_arr):
 ax[0].set_ylabel('Drain Source current $I_{DS}$ [$\mu$A]')
 
 plt.show()
-fig.savefig('img/fig_a.pdf')
+# fig.savefig('img/fig_a.png', dpi=300)
 
 
 
@@ -54,7 +64,7 @@ for i, I_, I_0, V_DS in zip([0, 1], [I_b, I], [I_0K_b, I_0K], [V_DS_arr_b, V_DS_
     ax.set_ylabel('Drain Source current $I_{DS}$ [$\mu$A]')
 
     plt.show()
-    fig.savefig(f'img/fig_b_{i}.pdf')
+#     fig.savefig(f'img/fig_b_{i}.png', dpi=300)
 
 
 
@@ -66,20 +76,20 @@ T_arr_c = np.array([298.])
 
 U_iter_c, U_c = load_U_iter('U_iter_c', T_arr_c, V_DS_arr_c, V_G_arr_c, U_guess=U_guess, n_iter=n_iter_c)
 I_c = get_I(U_c, T_arr_c, V_DS_arr_c)
-g_m = np.diff(I_c[0, 0, 0, :]) / np.diff(V_G_arr_c)
+g_m = np.diff(np.log10(I_c[0, 0, 0, :])) / np.diff(V_G_arr_c)
 
 fig, ax = plt.subplots(figsize=(5, 5), tight_layout=True)
 ax.plot(V_G_arr_c, I_c[0, 0, 0, :], label=f'$V_{{DS}}$ = {V_DS_arr_c[0]:.2f} V')
 # plot the derivative
-# ax.plot(V_G_arr2[1:], g_m/1e-6, label=f'$g_m$')
+ax.plot(V_G_arr_c[1:], np.log10(g_m), label=f'$g_m$')
 ax.legend()
 ax.set_xlabel('Gate bias $V_G$ [V]')
 ax.set_ylabel('Drain Source current $I_{DS}$ [A]')
 ax.set_title(f'$T$ = {T_arr_c[0]} K')
 ax.grid()
-ax.set_yscale('log')
+#ax.set_yscale('log')
 # ax.text(0.05, 0.01, f'$g_{{m}}$ = {:.2}')
-fig.savefig('img/fig_c.pdf')
+# fig.savefig('img/fig_c.png', dpi=300)
 
 plt.show()
 
